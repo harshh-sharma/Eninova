e9749037432e053318a8f58851417f9133b32c0b
Harsh Sharma <124427013+harshh-sharma@users.noreply.github.com>
2024-12-06 16:28:23 +0530

    working oon blog page

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,17 +1,62 @@
 import React from 'react'
 
 const BlogCard = () => {
-    return <div className='w-[983px] h-[640px] border-2 border-red-300'>
-
-    </div>
-}
-
-const page = () => {
   return (
-    <div className='w-full h-[4665px] border-2 border-black flex justify-center items-center'>
-         <BlogCard/>
+    <div className="w-full max-w-[983px] h-auto md:h-[640px] flex flex-col justify-center items-center relative mx-auto my-5">
+      <div className="w-full h-[500px] bg-[#A8DCFF] flex flex-col justify-center items-center py-6 md:h-[451px] border-[3px] md:border-[5px] border-[#172A44]">
+        {/* Top Image */}
+        <div className="w-[90%] max-w-[852px] h-auto md:h-[300px] absolute -top-16 md:-top-8">
+          <img
+            src="/featureImage.png"
+            alt="feature"
+            className="w-full h-full object-cover"
+          />
+        </div>
+
+        {/* Text Content */}
+        <div className="w-full max-w-[852px] mt-[100px] md:mt-[230px] px-4 pb-[20px]">
+          <h1 className="text-[24px] md:text-[32px] font-[600] leading-[1.2] text-center text-[#172A44]">
+            Lorem ipsum dolor sit amet.
+          </h1>
+          <p className="text-center text-[16px] md:text-[19px] leading-[1.5] md:leading-[35px] font-[500] text-[#172A44] mt-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+            occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            mollit anim id est laborum.
+          </p>
+
+          <div className='flex justify-center items-center py-[5px] md:pb-10'>
+          <button className='text-[#53BC69] text-[14px] md:text-[17px] font-[600] text-center underline'>Go to Project</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default page
+const Page = () => {
+  return (
+    <div className='flex flex-col'>
+       <h1 className="text-[#172A44] text-[60px] text-center font-semibold mb-[20px] drop-shadow-lg">
+  Blog
+</h1>
+
+    <div className="w-full min-h-screen flex flex-col justify-center items-center  space-y-[100px] md:space-y-[10px] py-6 px-4">
+      
+      {/* Adding spacing between BlogCards */}
+      <BlogCard />
+      <BlogCard />
+      <BlogCard />
+      <BlogCard />
+      <BlogCard />
+      <BlogCard />
+      <BlogCard />
+    </div>
+    </div>
+  )
+}
+
+export default Page
--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -96,63 +96,183 @@ const Page = () => {
       </div>
 
       {/* another */}
-      <div className="w-[1440px] h-[517px]  bg-[#F6F6F6] flex  justify-center items-center">
-        <div className="w-[1365px] h-[429px]  flex flex-col justify-center items-center">
-          <div className="flex gap-[50px] my-10 justify-center items-center">
-            {firstLevelCard?.map(item => <Card/>)}
-          </div>
+      <div className="w-full max-w-[1440px] h-auto bg-[#F6F6F6] flex justify-center items-center px-4">
+  <div className="w-full max-w-[1365px] h-auto flex flex-col justify-center items-center">
+    
+    {/* First Level Cards */}
+    <div className="flex flex-wrap gap-[50px] my-10 justify-center items-center">
+      {firstLevelCard?.map(item => (
+        <Card key={item.id} />
+      ))}
+    </div>
 
-          <div className="flex gap-[50px] my-10 justify-center items-center">
-            {secondLevelCard?.map(item => <Card/>)}
-            {/* <div className="w-[197.12px] h-[94.12px] border-2 border-green-500"></div> */}
-          </div>
-        </div>
-      </div>
+    {/* Second Level Cards */}
+    <div className="flex flex-wrap gap-[50px] my-10 justify-center items-center">
+      {secondLevelCard?.map(item => (
+        <Card key={item.id} />
+      ))}
+    </div>
+  </div>
+</div>
+
 
       {/* another */}
-      <div className="w-full flex justify-center items-center">
-      <div className="h-[694.29px] w-[1370px]  flex items-center justify-center flex-col">
-            <p className="text-[60px] font-600 text-[#172A44] text-center">Why <span className="text-[#53BC69]">Us!!</span></p>
+      <div className="w-full flex justify-center items-center px-4">
+  <div className="h-auto max-w-[1370px] w-full flex items-center justify-center flex-col">
+    <p className="text-[40px] sm:text-[50px] md:text-[60px] font-semibold text-[#172A44] text-center">
+      Why <span className="text-[#53BC69]">Us!!</span>
+    </p>
 
-            <p className="text-[40px] font-600 text-[#172A44] text-center mt-5">Business growth through Innovation</p>
+    <p className="text-[30px] sm:text-[35px] md:text-[40px] font-semibold text-[#172A44] text-center mt-5">
+      Business growth through Innovation
+    </p>
 
-           <div className="w-[1160px] h-[133px] flex justify-center items-center">
-           <p className="text-center text-[24px] font-500 leading-[48px] text-[#172A44] ">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis sint cupiditate accusamus tempore, deleniti nemo ducimus. Labore voluptatum sequi molestiae!</p>
-           </div>
+    <div className="w-full max-w-[1160px] h-auto flex justify-center items-center px-4">
+      <p className="text-center text-[16px] sm:text-[18px] md:text-[24px] font-medium leading-[30px] sm:leading-[36px] text-[#172A44]">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis sint cupiditate accusamus tempore, deleniti nemo ducimus. Labore voluptatum sequi molestiae!
+      </p>
+    </div>
 
-           <div className="flex justify-center gap-5 my-[50px]">
-            <div className="w-[291px] h-[265.4px]  flex justify-center items-center flex-col gap-5 px-2">
-                <img src="/Ad.png" alt=""/>
-                <p className="text-center font-700 text-[20px] text-[#172A44]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, corporis optio! Exercitationem ipsum atque in adipisci. Sapiente odit ducimus repellendus?</p>
-            </div>
-            <div  className="w-[291px] h-[265.4px] flex justify-center items-center flex-col gap-5 px-2">
-                <img src="/growth.png" alt="" />
-                <p className="text-center font-700 text-[20px] text-[#172A44]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, corporis optio! Exercitationem ipsum atque in adipisci. Sapiente odit ducimus repellendus?</p>
-            </div>
-            <div  className="w-[291px] h-[265.4px] flex justify-center items-center flex-col gap-5 px-2">
-                <img src="/bag.png" alt="" />
-                <p className="text-center font-700 text-[20px] text-[#172A44]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, corporis optio! Exercitationem ipsum atque in adipisci. Sapiente odit ducimus repellendus?</p> 
-            </div>
+    <div className="flex flex-wrap justify-center gap-6 my-[50px]">
+      <div className="w-full sm:w-[291px] h-auto flex justify-center items-center flex-col gap-4 px-4">
+        <img src="/Ad.png" alt="Ad" className="w-full max-w-[100px] h-auto object-cover" />
+        <p className="text-center font-semibold text-[16px] sm:text-[18px] md:text-[20px] text-[#172A44]">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, corporis optio! Exercitationem ipsum atque in adipisci. Sapiente odit ducimus repellendus?
+        </p>
+      </div>
 
-           </div>
+      <div className="w-full sm:w-[291px] h-auto flex justify-center items-center flex-col gap-4 px-4">
+        <img src="/growth.png" alt="Growth" className="w-full max-w-[100px] h-auto object-cover" />
+        <p className="text-center font-semibold text-[16px] sm:text-[18px] md:text-[20px] text-[#172A44]">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, corporis optio! Exercitationem ipsum atque in adipisci. Sapiente odit ducimus repellendus?
+        </p>
+      </div>
+
+      <div className="w-full sm:w-[291px] h-auto flex justify-center items-center flex-col gap-4 px-4">
+        <img src="/bag.png" alt="Bag" className="w-full max-w-[100px] h-auto object-cover" />
+        <p className="text-center font-semibold text-[16px] sm:text-[18px] md:text-[20px] text-[#172A44]">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, corporis optio! Exercitationem ipsum atque in adipisci. Sapiente odit ducimus repellendus?
+        </p>
+      </div>
+    </div>
+  </div>
 </div>
 
+
+
+
+
+
+      {/* another section */}
+      <div className="hidden md:block">
+      <div className="w-[1154px] h-[565px]  flex relative">
+  {/* Red Div */}
+  <div className="w-[510px] h-[510px]  absolute z-10">
+    <img src="/profile1.png" alt="" className="w-full h-full object-cover" />
+  </div>
+  
+  {/* Yellow Div */}
+  <div className="w-[983px] h-[428px] bg-[#172A44] absolute bottom-0 right-0 z-0 px-5 flex justify-end p-[30px]">
+  <div className="w-[557px] h-[342px]  flex flex-col justify-center items-center">
+    <h1 className="text-[32px] font-semibold text-white ">
+      Lorem ipsum dolor sit amet consectetur.
+    </h1>
+    <p className="text-[20px] font-light text-white ">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+  </div>
+</div>
+
+</div>
+
+
+<div className="w-[1154px] h-[565px] flex relative my-[100px]">
+  {/* Red Div */}
+  <div className="w-[510px] h-[510px]  absolute right-0 z-10">
+    <img src="/profile2.png" alt="" className="w-full h-full object-cover" />
+  </div>
+  
+  {/* Yellow Div */}
+  <div className="w-[983px] h-[428px] bg-[#172A44] absolute bottom-0 left-0 z-0 p-[30px] ">
+    <div className="w-[557px] h-[342px] flex justify-center items-center flex-col py-[5px]">
+        <h1 className="text-[32px] font-600 text-white">Lorem ipsum dolor sit amet consectetur.</h1>
+        <p className="text-[20px] font-400 text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.!</p>
+    </div>
+  </div>
+</div>
       </div>
+
+      <div className="md:hidden">
+  <div className="w-full h-auto flex flex-col relative">
+    {/* Red Div */}
+    <div className="w-full h-[250px] sm:h-[300px] mb-4 relative">
+      <img src="/profile1.png" alt="Profile 1" className="w-full h-full object-cover" />
+    </div>
+
+    {/* Yellow Div */}
+    <div className="w-full bg-[#172A44] p-6 flex flex-col items-center text-white">
+      <div className="max-w-[557px] w-full">
+        <h1 className="text-[28px] sm:text-[32px] font-semibold mb-4">
+          Lorem ipsum dolor sit amet consectetur.
+        </h1>
+        <p className="text-[16px] sm:text-[20px] font-light">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+    </div>
+  </div>
+
+
+<div className="w-full h-auto flex flex-col my-[100px]">
+  {/* Red Div */}
+  <div className="w-full h-[250px] sm:h-[300px] mb-4 relative">
+    <img src="/profile2.png" alt="Profile 2" className="w-full h-full object-cover" />
+  </div>
+
+  {/* Yellow Div */}
+  <div className="w-full bg-[#172A44] p-6 flex flex-col items-center text-white">
+    <div className="max-w-[557px] w-full">
+      <h1 className="text-[28px] sm:text-[32px] font-semibold mb-4">
+        Lorem ipsum dolor sit amet consectetur.
+      </h1>
+      <p className="text-[16px] sm:text-[20px] font-light">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </div>
+  </div>
+</div>
+</div>
+
 
       {/* another */}
-      <div className="w-[1154px] h-[1255px] border-2 border-black  flex flex-col justify-between">
-          <div className="w-full h-[567px] border-red-400 border-2">
-              <div className="w-[983px] h-[482px] border-2 border-yellow-500 relative bottom-0">
+      <div className="max-w-[1440px] w-full mx-auto px-4 py-8">
+  <p className="text-center text-[40px] md:text-[60px] font-bold text-[#172A44] leading-tight">
+    Our Awards and <span className="text-[#53BC69]">Achievements</span>
+  </p>
 
-              </div>
-          </div>
-          <div className="w-full h-[567px] border-red-400">
-          <div className="w-[983px] h-[482px] border-2 border-yellow-500  relative bottom-0">
+  <div className="flex flex-col md:flex-row justify-around items-center mt-8 gap-8">
+    {/* Text Section */}
+    <div className="max-w-[495px] w-full h-auto  flex flex-col gap-4 p-5">
+      <h1 className="text-[24px] md:text-[36px] font-semibold leading-[1.2]">
+        Lorem ipsum dolor sit amet.
+      </h1>
+      <p className="text-[18px] md:text-[24px] leading-[1.5] font-medium">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit pariatur natus facilis dolor ducimus et obcaecati sapiente dicta optio sequi.
+      </p>
+    </div>
 
+    {/* Image Section */}
+    <div className="w-full max-w-[520px] h-auto">
+      <img
+        src="/award.png"
+        alt="award"
+        className="w-full h-full object-cover rounded-lg shadow-lg"
+      />
+    </div>
+  </div>
 </div>
 
-</div>
-      </div>
     </div>
   );
 };
