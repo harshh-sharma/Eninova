c326161e5d557148d13163f48bafa713b5353024
Harsh Sharma <124427013+harshh-sharma@users.noreply.github.com>
2024-12-08 14:16:50 +0530

    adding dynamic content

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,13 +1,39 @@
-import React from 'react'
+"use client";
+
+import axiosInstance from '@/helper/axiosInstance'
+import React, { useEffect, useState } from 'react'
 
 const BlogCard = () => {
+
+  const [blogContent,setBlogContent] = useState([]);
+  console.log("blogcontent",blogContent);
+  
+
+  const loadContents = async () => {
+    try {
+      const response = await axiosInstance.get(`domain/67554695d770ca907259c7e5`);
+      console.log('response',response);
+      setBlogContent(response?.data?.contents);
+
+      
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
+
+
+  useEffect(() => {
+    loadContents();
+  },[])
+
   return (
     <div className="w-full max-w-[983px] h-auto md:h-[640px] flex flex-col justify-center items-center relative mx-auto my-5">
       <div className="w-full h-[500px] bg-[#A8DCFF] flex flex-col justify-center items-center py-6 md:h-[451px] border-[3px] md:border-[5px] border-[#172A44]">
         {/* Top Image */}
         <div className="w-[90%] max-w-[852px] h-auto md:h-[300px] absolute -top-16 md:-top-8">
           <img
-            src="/featureImage.png"
+            src={blogContent?.length > 0 ? blogContent[0]?.contentImage?.secure_url :'/featureImage.png'}
             alt="feature"
             className="w-full h-full object-cover"
           />
@@ -16,16 +42,16 @@ const BlogCard = () => {
         {/* Text Content */}
         <div className="w-full max-w-[852px] mt-[100px] md:mt-[230px] px-4 pb-[20px]">
           <h1 className="text-[24px] md:text-[32px] font-[600] leading-[1.2] text-center text-[#172A44]">
-            Lorem ipsum dolor sit amet.
+            {blogContent?.length > 0 ? blogContent[0]?.title : `Lorem ipsum dolor sit amet.`}
           </h1>
           <p className="text-center text-[16px] md:text-[19px] leading-[1.5] md:leading-[35px] font-[500] text-[#172A44] mt-4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            {blogContent?.length > 0 ? blogContent[0]?.content :  `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
             veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
             commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
             velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
             occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-            mollit anim id est laborum.
+            mollit anim id est laborum.`}
           </p>
 
           <div className='flex justify-center items-center py-[5px] md:pb-10'>
---
+++ src/helper/axiosInstance.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+// https://website-content-management-system.onrender.com
+
+const BASE_URL = "https://website-content-management-system.onrender.com/api/v1";
+
+const axiosInstance = axios.create();
+axiosInstance.defaults.baseURL = BASE_URL;
+axiosInstance.defaults.withCredentials = false;
+
+export default axiosInstance;
