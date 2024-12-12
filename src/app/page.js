e04f81adb738e7ad94ec80a56cdb68eff8c1fcd5
Harsh Sharma <124427013+harshh-sharma@users.noreply.github.com>
2024-12-12 12:33:31 +0530

    correct the issue

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import { FaWifi } from "react-icons/fa";
 import { FiTv } from "react-icons/fi";
 import { HiSpeakerWave } from "react-icons/hi2";
 import Image from "next/image";
-import ClientCarousel from "../components/Client";
+import ClientCarousel from "../components/client";
 import OurExperts from "../components/Experts";
 import Contact from "../components/Contact";
 import GetInTouch from "../components/GetInTouch";
